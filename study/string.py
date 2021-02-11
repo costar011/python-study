69d7da2d65e64ad0605ed8a99c635b77af2bcbb7
YERIM <dnaer456@gmail.com>
2021-02-12 00:07:07 +0900

    030

--- study/string.py
+++ study/string.py
@@ -81,3 +81,10 @@ print(url_split[-1])
 string = 'abcdfe2a354a32a'
 string = string.replace('a', 'A')
 print(string)
+
+# 030 replace 메소드
+# 아래 코드의 실행 결과를 예상
+# string = 'abcd'
+# string.replace('b', 'B')
+# print(string)
+# abcd가 그대로 출력. 문자열은 변경할 수 없는 자료형이기 때문이다. replace 메서드를 사용하면 원본은 그대로 둔채로 변경된 새로운 문자열 객체를 리턴해준다.
