e0047e7cd3e7adcb08b39d89074467ed124a2a95
costar011 <dnaer456@gmail.com>
2021-04-27 19:44:29 +0900

    068

--- study/list2.py
+++ study/list2.py
@@ -43,3 +43,39 @@ print(interest[0], interest[2])
 # 출력 예시: 삼성전자 LG전자 Naver 카카오 Apple
 interest = ['삼성전자', 'LG전자', 'Naver', '카카오', 'Apple']
 print(" ".join(interest))
+
+
+# 066
+# join 메소드
+# interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+# interest = ['삼성전자', 'LG전자', 'Naver', '카카오', 'Apple']
+# interest 리스트를 사용하여 아래와 같이 화면에 출력
+# 출력 예시: 삼성전자/LG전자/Naver/카카오/Apple
+# 문자열의 join 메서드를 사용하면 리스트를 문자열로 붙일 수 있다.
+interest = ['삼성전자', 'LG전자', 'Naver', '카카오', 'Apple']
+print("/".join(interest))
+
+# 067
+# join 메소드
+# interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+# interest = ['삼성전자', 'LG전자', 'Naver', '카카오', 'Apple']
+# join() 메서드를 사용해서 interest 리스트를 아래와 같이 화면에 출력
+# 출력 예시:
+# 삼성전자
+# LG전자
+# Naver
+# 카카오
+# Apple
+interest = ['삼성전자', 'LG전자', 'Naver', '카카오', 'Apple']
+print("\n".join(interest))
+
+# 068
+# 문자열 split 메소드
+# 회사 이름이 슬래시 ('/')로 구분되어 하나의 문자열로 저장
+# string = "Apple/카카오/Naver"
+# 이를 interest 이름의 리스트로 분리 저장
+# 실행 예시 >> print(interest)
+# ['Apple', '카카오', 'Naver']
+string = "Apple/카카오/Naver"
+interest = string.split("/")
+print(interest)
