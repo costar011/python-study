6039a5710b419ee97d4d0ab5f624b5c423b9431b
costar011 <dnaer456@gmail.com>
2021-05-14 22:16:46 +0900

    tuple

--- study/tuple.py
+++ study/tuple.py
@@ -1,7 +1,14 @@
-# 070 tuple
+# 071 tuple
 # my_variable 이름의 비어있는 튜플을 만들어라.
 # 괄호는 튜플을 정의하는 기호
-# my_variable = ()
-# 정말 튜플이 생성됐는지 확인한다.
+my_variable = ()
 # type() 함수는 변수에 바인딩된 데이터의 타입을 반환
-# >> print(type(my_variable))
+print(type(my_variable))
+
+# 072
+# 영화 제목을 movie_rank 이름의 튜플에 저장 (순위 정보는 저장하지 않는다.)
+# 순위	 영화
+#  1	닥터 스트레인지
+#  2	스플릿
+#  3	럭키
+movie_rank = ("닥터 스트레인지", "스플릿", "럭키")
