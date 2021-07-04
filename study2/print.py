80318f3df7b94b0b453145191111f2ced0d42111
costar011 <dnaer456@gmail.com>
2021-07-04 18:18:07 +0900

    14

--- study2/print.py
+++ study2/print.py
@@ -82,3 +82,18 @@ print(num_int+1, type(num_int))
 num = 100
 num2 = str(num)
 print(num2, type(num2))
+
+# 13
+# 문자열 "15.79"를 실수(float) 타입으로 변환해보세요.
+str1 = "15.79"
+str1 = float(str1)
+print(str1, type(str1))
+
+# 14
+# year라는 변수가 문자열 타입의 연도를 바인딩하고 있습니다. 이를 정수로 변환한 후 최근 3년의 연도를 화면에 출력해보세요.
+# year = "2020"
+year = "2020"
+
+print(int(year)-1)  # 2019
+print(int(year)-2)  # 2018
+print(int(year)-3)  # 2017
