f94bd863978e92162b8d5aa2fa05f555a3e3932c
costar011 <dnaer456@gmail.com>
2021-07-07 00:10:06 +0900

    25

--- study2/print.py
+++ study2/print.py
@@ -171,3 +171,27 @@ print(result)
 url = "http://sharebook.kr"
 result = url.split(".")
 print(result[-1])
+
+# 23
+# 아래 문자열에서 소문자 'a'를 대문자 'A'로 변경하세요.
+# string = 'abcdfe2a354a32a'
+# 실행 예: Abcdfe2A354A32A
+string = 'abcdfe2a354a32a'
+string = string.replace('a', 'A')
+print(string)
+
+# 24
+# 화면에 '-'를 80개 출력하세요.
+# 실행 예: --------------------------------------------------------------------------------
+print("-" * 80)
+
+# 25
+# 변수에 다음과 같은 문자열이 바인딩되어 있습니다.
+# t1 = 'python'
+# t2 = 'java'
+# 변수에 문자열 더하기와 문자열 곱하기를 사용해서 아래와 같이 출력해보세요.
+# 실행 예: python java python java python java python java
+t1 = "python"
+t2 = "java"
+result = t1 + ' ' + t2 + ' '
+print(result * 4)
