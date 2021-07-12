facf4138ff801c7df740ee6a79d57896acf1f2ab
costar011 <dnaer456@gmail.com>
2021-07-12 17:40:33 +0900

    3

--- study2/print2.py
+++ study2/print2.py
@@ -2,3 +2,17 @@
 # 문자열 'hello'가 있을 때 이를 'Hello'로 변경해보세요.
 a = "hello"
 a = a.capitalize()
+
+# 2
+# 파일 이름이 문자열로 저장되어 있을 때 endswith 메서드를 사용해서 파일 이름이 'xlsx'로 끝나는지 확인해보세요.
+# endswith 메소드 활용
+# file_name = "보고서.xlsx"
+file_name = "보고서.xlsx"
+file_name.endswith("xlsx")
+
+# 3
+# 다음과 같은 문자열이 있을 때 공백을 기준으로 문자열을 나눠보세요.
+# a = "hello world"
+a = "hello world"
+a.split()
+# 문자열의 split() 메서드를 사용하면 문자열에서 공백을 기준으로 분리해줍니다.
