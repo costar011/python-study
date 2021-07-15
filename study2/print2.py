0021738561ef5073b60a24a05f87a89f6f93b1bc
costar011 <dnaer456@gmail.com>
2021-07-16 01:11:19 +0900

    4

--- study2/print2.py
+++ study2/print2.py
@@ -16,3 +16,11 @@ file_name.endswith("xlsx")
 a = "hello world"
 a.split()
 # 문자열의 split() 메서드를 사용하면 문자열에서 공백을 기준으로 분리해줍니다.
+
+# 4
+# 다음과 같이 문자열이 있을 때 btc와 krw로 나눠보세요.
+# ticker = "btc_krw"
+# 문자열에서 split() 메서드는 문자열을 분리할 때 사용합니다.
+# 이때 어떤 값을 넘겨주면 그 값을 기준으로 문자열을 분리해줍니다.
+ticker = "btc_krw"
+ticker.split("_")
