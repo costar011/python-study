693e7331d6946eb52a318d565cdb7aa0e4d64179
costar011 <dnaer456@gmail.com>
2021-08-08 00:01:16 +0900

    10

--- study2/print2.py
+++ study2/print2.py
@@ -52,3 +52,19 @@ movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
 movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
 movie_rank.append("아이언맨")
 print(movie_rank)
+
+# 9
+# movie_rank 리스트에는 아래와 같이 네 개의 영화 제목이 바인딩되어 있다.
+# "슈퍼맨"을 "닥터 스트레인지"와 "스플릿" 사이에 추가하라.
+# movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '아이언맨']
+# 리스트의 insert(인덱스, 원소) 메소드를 사용하면 특정 위치에 값을 끼어넣기 할 수 있다.
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '아이언맨']
+movie_rank.insert(1, "슈퍼맨")
+print(movie_rank)
+
+# 10
+# movie_rank 리스트에서 '럭키'를 삭제하라.
+# movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '아이언맨']
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '아이언맨']
+del movie_rank[3]
+print(movie_rank)
