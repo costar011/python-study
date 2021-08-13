40cee0ba64061f019b8c56050dd8344da448bc4c
costar011 <dnaer456@gmail.com>
2021-08-13 21:10:35 +0900

    11

--- study2/print2.py
+++ study2/print2.py
@@ -68,3 +68,13 @@ print(movie_rank)
 movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '아이언맨']
 del movie_rank[3]
 print(movie_rank)
+
+# 11
+# lang1과 lang2 리스트가 있을 때 lang1과 lang2의 원소를 모두 갖고 있는 langs 리스트를 만들어라.
+# lang1 = ["C", "C++", "JAVA"]
+# lang2 = ["Python", "Go", "C#"]
+# langs ['C', 'C++', 'JAVA', 'Python', 'Go', 'C#']
+lang1 = ["C", "C++", "JAVA"]
+lang2 = ["Python", "Go", "C#"]
+langs = lang1 + lang2
+print(langs)
