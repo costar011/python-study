19a1739a75d1f3ae30105fb6f038a7627684f42f
costar011 <dnaer456@gmail.com>
2021-08-30 21:23:32 +0900

    join 4

--- study2/join.py
+++ study2/join.py
@@ -14,3 +14,28 @@ print(" ".join(interest))
 # 문자열의 join 메서드를 사용하면 리스트를 문자열로 붙일 수 있습니다.
 interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
 print("/".join(interest))
+
+# 3
+# interest 리스트에는 아래의 데이터가 바인딩되어 있다.
+# interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+# join() 메서드를 사용해서 interest 리스트를 아래와 같이 화면에 출력하라.
+
+# 출력 예시
+# 삼성전자
+# LG전자
+# Naver
+# SK하이닉스
+# 미래에셋대우
+interest = ['삼성전자', 'LG전자', 'Naver', 'SK하이닉스', '미래에셋대우']
+print("\n".join(interest))
+
+# 4
+# 회사 이름이 슬래시 ('/')로 구분되어 하나의 문자열로 저장되어 있다.
+# string = "삼성전자/LG전자/Naver"
+# 이를 interest 이름의 리스트로 분리 저장
+# 실행 예시
+# print(interest)
+# ['삼성전자', 'LG전자', 'Naver']
+string = "삼성전자/LG전자/Naver"
+interest = string.split("/")
+print(interest)
