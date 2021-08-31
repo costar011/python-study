5b23066e9b2ba3bb376ecfc8a3a81222e38eb8c2
costar011 <dnaer456@gmail.com>
2021-08-31 20:26:37 +0900

    tuple

--- study2/join.py
+++ study2/join.py
@@ -50,3 +50,11 @@ print(data)
 data = [2, 4, 3, 1, 5, 10, 9]
 data2 = sorted(data)
 print(data2)
+
+# 6
+# my_variable 이름의 비어있는 튜플을 만들어라.
+# 괄호는 튜플을 정의하는 기호
+my_variable = ()
+# 정말 튜플이 생성됐는지 확인해 봅시다. type() 함수는 변수에 바인딩된 데이터의 타입을 반환
+print(type(my_variable))
+# 실행을 시키면 <class 'tuple'> 이 나온다.
