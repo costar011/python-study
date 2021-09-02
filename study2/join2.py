3cc9b63834c9e72cd3942f5fbeae9ea4ffb54895
costar011 <dnaer456@gmail.com>
2021-09-02 20:55:26 +0900

    join2

--- study2/join2.py
+++ study2/join2.py
@@ -6,3 +6,10 @@ data = list(interest)
 # 2
 # 다음 리스트를 튜플로 변경
 interest = ['삼성전자', 'LG전자', 'SK Hynix']
+data = tuple(interest)
+
+# 3
+# 1 부터 99까지의 정수 중 짝수만 저장된 튜플을 생성
+# (2, 4, 6, 8 ... 98)
+data = tuple(range(2, 100, 2))
+print(data)
